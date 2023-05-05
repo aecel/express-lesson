b41c5d265ad6d5b870d3ac4f75db97fc6a7e9f99
aecel <aecelacuna@gmail.com>
2023-05-05 22:07:06 +0800

    Add res things to server.js

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const app = express()
 
 app.get('/', (req, res) => {
     console.log('Here')
+
+    // Insert different res thingies here
+
+    // Outputs "Hi" on browser
     res.send('Hi')
+
+    // Send status code
+    // res.sendStatus(500)
 })
 
 app.listen(3000)
